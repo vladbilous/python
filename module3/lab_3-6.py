fc37bd585fa3995fba58521cc2d03f7f6f6adfa4
Vlad Bilous <vladbilous17@gmail.com>
2021-10-13 18:58:08 +0300

    add lab 3.6 -3.7

--- module3/lab_3-6.py
+++ module3/lab_3-6.py
@@ -6,6 +6,7 @@ dictCountries = {
     'Poland': 'PL',
     'Canada': 'CA'
 }
+
 # create another dict called capitals, where values of countries will be keys, and values will be the capitals i.e. {'UA': 'Kiev'}
 dictCapitals = {
     'UA': 'Kiev',
@@ -14,16 +15,33 @@ dictCapitals = {
     'PL': 'Warsaw',
     'CA': 'Ottawa'
 }
+
 # add one more element to each dict above
 dictCountries['Germany'] = 'GE'
 dictCapitals['GE'] = 'Berlin'
+
 # print sentence "Domain for COUNTRY is DOMAIN." for each record in countries where DOMAIN and COUNTRY have to be replaced with .format()
-for key, country in dictCountries.items():
-    sentenceFormat = "Domain for {} is {}.".format(key, country)
+print('-----------------Countries----------------------')
+for country, key in dictCountries.items():
+    sentenceFormat = "Domain for {} is {}.".format(country, key)
     print(sentenceFormat)
 
 # print sentence "The capital of COUNTRY is CAPITAL" for each record in capitals where COUNTRY and CAPITAL have to be replaced with .format()
+print('\n-----------------Capitals ----------------------')
+for key, capital in dictCapitals.items():
+    sentenceFormat = "The capital of {} is {}.".format(key, capital)
+    print(sentenceFormat)
 
 # Merge sentences above together with one cycle
+print('\n----------------- Merge sentences above together with one cycle ----------------------')
+for countries, value in dictCountries.items():
+    togetherCycle = "Domain for {} is {}. The capital is {}".format(countries, value, dictCapitals[value])
+    print(togetherCycle)
+
 # Add to each value of countries another two domains: COM and GOV, as they can be used too. So value will became a list
+print('Add to each value of countries another two domains: COM and GOV')
+for key, value in dictCountries.items():
+    newTwoDomains = dictCountries[key] = [value, 'COM', 'GOV']
+    print(newTwoDomains)
+
 # Answer on the questions how embedded data structures works, i.e. list as a value of dictionary.
